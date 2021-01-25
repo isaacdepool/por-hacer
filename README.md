3e67e74332e58e1feb110ba40b0a8f03724a0e60
IsaacDepool <isaacdepoolg@gmail.com>
2021-01-25 18:40:00 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicacion de comandos
+
+Este es un ejercicio del cuso de node
+
+Instalar los paquetes de node 
+
+```
+npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    alias: 'd',
+    demand: false
+    }
+
+const completado = {
+    alias: 'c',
+    default: true
+}
+
+
+const argv = require('yargs')
+            .command('crear', 'Crea un elemento por hacer', {
+                descripcion
+            })
+            .command('actualizar','Actualiza el estado completado de una tarea',
+                      {
+                        descripcion,
+                          completado
+                      })
+                .command('borrar', 'Borra item de la lista', {
+                    descripcion
+                })
+                .help()
+                .argv;
+
+module.exports = {
+    argv
+}
